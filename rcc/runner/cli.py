8059ba0908ea7f02935b8e9987d10bf0a2ff2d39
Benjamin Sergeant <bsergeant@mz.com>
2020-02-29 09:52:21 -0800

    rcc cli fixed to work with send only api

--- rcc/runner/cli.py
+++ rcc/runner/cli.py
@@ -33,78 +33,17 @@ async def interpreter(redis_url, redis_password, args):
         cmd = args[0]
         cmd = cmd.upper()
 
-        if cmd == 'DEL':
-            key = args[1]
-            response = await client.delete(key)
-
-        elif cmd == 'PING':
-            response = await client.ping()
-            response = response.decode()
-
-        elif cmd == 'SET':
-            key = args[1]
-            value = args[2]
-            response = await client.set(key, value)
-            response = response.decode()
-
-        elif cmd == 'GET':
-            key = args[1]
-            response = await client.get(key)
-            response = response.decode()
-
-        elif cmd == 'EXISTS':
-            key = args[1]
-            response = await client.exists(key)
-
-        elif cmd == 'XREVRANGE':
-            key = args[1]
-            end = args[2]
-            start = args[3]
-            _ = args[4]  # COUNT
-            count = args[5]
-            response = await client.xrevrange(key, end, start, count)
-
-        elif cmd == 'CLUSTER':
-            subcommand = args[1]
-
-            if subcommand == 'NODES':
-                printed = True
-                nodes = await client.cluster_nodes()
-                for node in nodes:
-                    print(node)
-
-            elif subcommand == 'GETKEYSINSLOT':
-                slot = int(args[2])
-                count = int(args[3])
-                response = await client.cluster_get_keys_in_slot(slot, count)
-
-            elif subcommand == 'SETSLOT':
-                slot = int(args[2])
-                mode = args[3]
-                node_id = args[4]
-                response = await client.cluster_set_slot(slot, mode, node_id)
-
-        elif cmd == 'INFO':
-            response = await client.info()
-
-        elif cmd == 'UNKNOWN':
-            response = await client.unknown_command()
-
-        elif cmd == 'MIGRATE':
-            host = args[1]
-            port = args[2]
-            db = args[3]
-            timeout = args[4]
-            key = args[5]
-            response = await client.migrate(host, port, db, timeout, key)
-            response = response.decode()
-
+        if len(args) > 1:
+            cmdArgs = args[1:]
+            response = await client.send(cmd, *cmdArgs)
         else:
-            print(f'Unknown command {cmd}')
-            continue
+            response = await client.send(cmd)
 
         if not printed:
             print(response)
+
+        if not quit:
+            args = []
 
     client.close()
 
