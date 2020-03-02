3ba19e5199bcc4df00d24f62ab75f6c78b3f398c
Benjamin Sergeant <bsergeant@mz.com>
2020-03-01 18:35:17 -0800

    add option to generate random channel name for batch mode for publish cli command

--- rcc/runner/publish.py
+++ rcc/runner/publish.py
@@ -4,20 +4,25 @@ Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
 '''
 
 import asyncio
+import uuid
 from urllib.parse import urlparse
 
 import click
 from rcc.client import RedisClient
 
 
-async def pub(redis_url, redis_password, channel, msg, batch, maxLen):
+async def pub(redis_url, redis_password, channel, random_channel, msg, batch, maxLen):
     client = RedisClient(redis_url, redis_password)
     await client.connect()
 
     if batch:
         while True:
+            chan = channel
+            if random_channel:
+                chan = str(uuid.uuid4())
+
             streamId = await client.send(
-                'XADD', channel, 'MAXLEN', '~', maxLen, b'*', 'json', msg
+                'XADD', chan, 'MAXLEN', '~', maxLen, b'*', 'json', msg
             )
     else:
         streamId = await client.send(
@@ -31,10 +36,13 @@ async def pub(redis_url, redis_password, channel, msg, batch, maxLen):
 @click.option('--port', '-p')
 @click.option('--redis_password')
 @click.option('--channel', default='foo')
+@click.option('--random_channel', is_flag=True)
 @click.option('--msg', default='{"bar": "baz"}')
 @click.option('--batch', is_flag=True)
 @click.option('--max_len', default='100')
-def publish(redis_url, port, redis_password, channel, msg, batch, max_len):
+def publish(
+    redis_url, port, redis_password, channel, random_channel, msg, batch, max_len
+):
     '''Publish to a channel
     '''
 
@@ -44,5 +52,5 @@ def publish(redis_url, port, redis_password, channel, msg, batch, max_len):
         redis_url = f'redis://{host}:{port}'
 
     asyncio.get_event_loop().run_until_complete(
-        pub(redis_url, redis_password, channel, msg, batch, max_len)
+        pub(redis_url, redis_password, channel, random_channel, msg, batch, max_len)
     )
