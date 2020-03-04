bccb31cb07c7e78a8abac0fb6ff43704af4c1967
Benjamin Sergeant <bsergean@gmail.com>
2020-03-03 17:07:11 -0800

    add ability to select redis library used

--- cobras/server/redis_clients.py
+++ cobras/server/redis_clients.py
@@ -7,17 +7,20 @@ from cobras.server.redis_client import RedisClient
 
 
 class RedisClients(object):
-    def __init__(self, redisUrls, redisPassword, redisCluster, appsConfig):
+    def __init__(self, redisUrls, redisPassword, redisCluster, library, appsConfig):
         self.redisUrls = redisUrls
         self.redisPassword = redisPassword
         self.redisCluster = redisCluster
+        self.library = library
         self.clients = {}
 
         for app in appsConfig.apps:
             self.clients[app] = self.makeRedisClient()
 
     def makeRedisClient(self):
-        return RedisClient(self.redisUrls, self.redisPassword, self.redisCluster)
+        return RedisClient(
+            self.redisUrls, self.redisPassword, self.redisCluster, self.library
+        )
 
     def getRedisClient(self, appkey):
         return self.clients.get(appkey)
