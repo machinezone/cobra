2bf02a12cc8b81bbb94f809dfae650221ca90b2e
Benjamin Sergeant <bsergean@gmail.com>
2020-12-03 21:17:44 -0800

    pulsar mode: support published messages, written to redis

--- cobras/server/pulsar.py
+++ cobras/server/pulsar.py
@@ -5,6 +5,7 @@ Copyright (c) 2018-2020 Machine Zone, Inc. All rights reserved.
 
 import base64
 import json
+import logging
 from typing import Dict
 
 from cobras.common.cobra_types import JsonDict
@@ -12,9 +13,51 @@ from cobras.server.connection_state import ConnectionState
 
 
 async def badFormat(state: ConnectionState, ws, app: Dict, reason: str):
-    response = {"body": {"error": "bad_schema", "reason": reason}}
+    response = {"result": "error", "reason": reason}
     state.ok = False
     state.error = response
+    await state.respond(ws, response)
+
+
+async def handlePublishMessage(
+    state: ConnectionState, ws, app: Dict, pdu: JsonDict, serializedPdu: str, path: str
+):
+    tokens = path.split('/')
+    if len(tokens) != 8:
+        await badFormat(state, ws, app, f'Invalid uri -> {path}')
+        return
+
+    tenant = tokens[5]
+    namespace = tokens[6]
+    topic = tokens[7]
+
+    chan = f'{tenant}::{namespace}::{topic}'
+    payload = pdu.get('payload')
+    context = pdu.get('context')
+
+    appkey = state.appkey
+    redis = app['redis_clients'].getRedisClient(appkey)
+
+    try:
+        maxLen = app['channel_max_length']
+        stream = '{}::{}'.format(appkey, chan)
+        streamId = await redis.xadd(stream, 'json', payload, maxLen)
+    except Exception as e:
+        # await publishers.erasePublisher(appkey, chan)  # FIXME
+
+        errMsg = f'publish: cannot connect to redis {e}'
+        logging.warning(errMsg)
+        response = {
+            "action": "rtm/publish/error",
+            "id": pdu.get('id', 1),
+            "body": {"error": errMsg},
+        }
+        await state.respond(ws, response)
+        return
+
+    app['stats'].updateChannelPublished(chan, len(serializedPdu))
+
+    response = {"result": "ok", "messageId": streamId.decode(), "context": context}
     await state.respond(ws, response)
 
 
@@ -32,11 +75,7 @@ async def processPulsarMessage(
 
     state.log(f"< {serializedPdu}")
 
-    # Fake happy response
-    response = {
-        "action": f"youpi",
-        "id": pdu.get('id', 1),
-        "body": {"error": "ooooooo"},
-    }
-    await state.respond(ws, response)
-    return
+    if path.startswith('/ws/v2/producer'):
+        await handlePublishMessage(state, ws, app, pdu, serializedPdu, path)
+    else:
+        await badFormat(state, ws, app, f'Invalid endpoint: {path}')
